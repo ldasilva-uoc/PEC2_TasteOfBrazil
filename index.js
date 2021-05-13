918b6968103ee0539e6201776248f7a78ed1cbcb
ldasilva <ldasilva.uoc.edu>
2021-05-13 18:03:54 +0200

    js en todos los html

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ h1.onmouseout = function() {change_color_blue()};
 
 function change_color_yellow()
 {
-  
- 
+    h1.setAttribute("style", "color: #e2cb00");
+    span.setAttribute("style", "color: #002277");
 }
 
 function change_color_blue()
 {
-    
+    h1.setAttribute("style", "color: #002277");
+    span.setAttribute("style", "color: #e2cb00");
 }
